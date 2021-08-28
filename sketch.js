1b825cc1e4f0da753cca5c527df424a0af2778bb
Athrva05 <87930083+Athrva05@users.noreply.github.com>
2021-08-28 19:22:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+ var fristTakeAbowl;
+ var mixflour,milk,blueBerryEssence,bakingSoda,butter,powderSugar,purpleFoodColour;
+ var mixThem;
+ var putAButterPapperAtTheBottomOfCakeMould,greaseTheCakeMouldWithMeltedButter;
+ var nowPutTheMadeBatterIntoTheCakeMould;
+ var makeASolutionof3GramOfBaingSodaAnd15TableSpoonWater;
+ var nowTakeWokAndPutTheAboveMadeSolutionIntoIt;
+ var nowTakeHotPotStandAndKeepItInsideWok;
+ var nowPutTheMouldOnTheHotPotStandInsideWokAndPutALidOnIt;
+ var bakeIFor20to30Min;
+ var nowTakeOutTheLid;
+ var takeAToothPickNowAndPutItInsidetheCake;
+ var ifTheToothPickComesOutwithNoMaterialThenThinkThatYourCakeIsReadyFor,elseItNeedsToBeBakedForABitMoreTime;
+ var nowTakeSomeWaterInABowlAndPutSomePowderedSugarInIt;
+ var nowtakeAThreadAndCutTheCakeInto3EqualPieces;
+ var takeTheAboveMadeOfWaterAndSugarAndApplyItOnTheTopEveryLayer;
+ var nowIfYouLikeForIcingThenFollowTheseSteps;
+ var nowTake2BowlTheFirstBowlShouldBeSuchBigThatTheSecondBowlShouldPerfectlyFitInIt;
+ var nowPut14to15IceCubesIntoFirstBowl;
+ var takeACupOfWippedCreamAndPutItIntoSecondBowl;
+ var nowTakeABitterAndBitTheWippedCream;
+ var ifYouDoNotHaveABitterYouCanUseAWhiskToBitTheCream;
+ var iLoveToUseColourWipppedCream;
+ var asTheCakeIsInPurpleColourIWillUseToFoodColurBlue,Pink;
+ var iHaveSomeNozzlesAndIAmGoingUseThemToDecorateTheCake;
+ var afterDecoratingTheCakeKeepItIntoFridge;
+ var after2hrsYourCakeIsReadyToServe;
+  
+
+
+
+
+
+ 
+
+
+
+
+
